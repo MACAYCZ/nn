57db0130a81c5dd18c5a61a37138d275f420db77
Petr Holčák <petr7698@gmail.com>
2024-10-29 16:17:06 +0100

    Encapsulate backpropagation kernel within a layer object

--- test.cu
+++ test.cu
@@ -44,10 +44,11 @@ static __global__ void forward_fixed_layer(
 {
 	__shared__ float shrd_in[N_IN];
 	weights += (out_off + CUDA_THREAD_INDEX) * in_stride + in_off;
+	in += in_off;
 	out += out_off + CUDA_THREAD_INDEX;
 
 	// TODO(petr): Loop through all neurons, instead of performing an out-of-bounds check.
-	if (out_off + CUDA_THREAD_INDEX < out_stride)
+	if (out_off + CUDA_THREAD_INDEX < out_stride) // TODO(petr): It should compare against n_out, instead of out_stride.
 	{
 		const float bias = biases[out_off + CUDA_THREAD_INDEX];
 		for (std::uint32_t i = 0; i < batch_sz; i++)
@@ -67,6 +68,57 @@ static __global__ void forward_fixed_layer(
 			*out = result;
 			in += in_stride;
 			out += out_stride;
+		}
+	}
+}
+
+//
+// N_OUT represents the size of the current layer, while the number of threads
+// corresponds to the size of the previous layer.
+// Ensure that N_OUT * 4 is less than the available shared memory capacity.
+// N_OUT has to be equal to the number of threads per block.
+//
+template <std::uint32_t N_OUT, float(*IN_ACTIVATION_GRADIENT)(float) = _activation_stub>
+static __global__ void backward_fixed_layer(
+	const float *__restrict__ weights,
+	const float *__restrict__ gradients,
+	const float *__restrict__ in,
+	float *__restrict__ in_gradients,
+	const std::uint32_t in_stride,
+	const std::uint32_t in_off,
+	const std::uint32_t out_stride,
+	const std::uint32_t out_off,
+	const std::uint32_t batch_sz)
+{
+	// TODO(petr): Lower the number of registers used by each thread.
+	__shared__ float shrd_gradients[N_OUT];
+	weights += out_off * in_stride + in_off + CUDA_THREAD_INDEX;
+	gradients += out_off;
+	in += in_off + CUDA_THREAD_INDEX;
+	in_gradients += in_off + CUDA_THREAD_INDEX;
+
+	if (in_off + CUDA_THREAD_INDEX < in_stride) // TODO(petr): It should compare against n_in, instead of in_stride.
+	{
+		for (std::uint32_t i = 0; i < batch_sz; i++)
+		{
+			float in_gradient = in_off ? *in_gradients : 0.0f;
+
+			__syncthreads();
+			shrd_gradients[threadIdx.x] = gradients[threadIdx.x];
+			__syncthreads();
+
+			#pragma unroll
+			for (std::uint32_t j = 0; j < N_OUT; j++)
+			{
+				in_gradient += weights[j * in_stride] * shrd_gradients[j];
+			}
+
+			in_gradient *= IN_ACTIVATION_GRADIENT(*in);
+			*in_gradients = in_gradient;
+
+			in += in_stride;
+			in_gradients += in_stride;
+			gradients += out_stride;
 		}
 	}
 }
@@ -108,7 +160,7 @@ public:
 
 		for (std::uint32_t out_off = 0; out_off < this->n_out; out_off += 40*N_IN)
 		{
-			for (std::uint32_t in_off = 0; in_off < in_stride; in_off += N_IN)
+			for (std::uint32_t in_off = 0; in_off < this->n_in; in_off += N_IN)
 			{
 				forward_fixed_layer<N_IN, IN_ACTIVATION><<<40, N_IN>>>(
 					this->biases,
@@ -127,6 +179,33 @@ public:
 		return this->out;
 	}
 
+	template <float(*IN_ACTIVATION_GRADIENT)(float) = _activation_stub>
+	void backward(Layer& in_layer)
+	{
+		constexpr std::uint32_t N_OUT = 128;
+		std::uint32_t in_stride = (this->n_in + 127) & ~127;
+		std::uint32_t out_stride = (this->n_out + 127) & ~127; // TODO(petr): Handle out_stride everywhere.
+
+		for (std::uint32_t in_off = 0; in_off < this->n_in; in_off += 40*N_OUT)
+		{
+			for (std::uint32_t out_off = 0; out_off < this->n_out; out_off += N_OUT)
+			{
+				backward_fixed_layer<N_OUT, IN_ACTIVATION_GRADIENT><<<40, N_OUT>>>(
+					this->weights,
+					this->gradients,
+					in_layer.out,
+					in_layer.gradients,
+					in_stride,
+					in_off,
+					out_stride,
+					out_off,
+					this->batch_sz);
+			}
+		}
+
+		ASSERT_CUDA_ERROR();
+	}
+
 private:
 	std::uint32_t n_in;
 	std::uint32_t n_out;
@@ -143,6 +222,7 @@ int main(void)
 	std::uint32_t n_out = 40*n_in;
 	std::uint32_t batch_sz = 4096;
 	std::uint32_t n_epochs = 100;
+	Layer in_layer(28*28, n_in, batch_sz);
 	Layer layer(n_in, n_out, batch_sz);
 
 	// TODO(petr): Make the input allocation more convenient.
@@ -156,7 +236,8 @@ int main(void)
 	cudaEventRecord(start);
 	for (std::size_t epoch = 0; epoch < n_epochs; epoch++)
 	{
-		layer.forward<>(in);
+//		layer.forward<>(in);
+		layer.backward<>(in_layer);
 	}
 	cudaEventRecord(stop);
 
