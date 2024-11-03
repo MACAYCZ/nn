07600b45bf4409dbd81ac9225613af6c4541db7b
Petr Holčák <petr7698@gmail.com>
2024-11-03 10:32:15 +0100

    Format shell.nix using nixfmt

--- shell.nix
+++ shell.nix
@@ -1,16 +1,13 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
-  pkgs = import nixpkgs {
+{
+  pkgs ? import <nixpkgs> {
     config.allowUnfree = true;
     config.cudaSupport = true;
-  };
-in
-  pkgs.mkShell.override {
-    stdenv = pkgs.gcc12Stdenv;
-  } {
-    packages = with pkgs; [
-      cmake
-      cudaPackages.cudatoolkit
-    ];
-    LD_LIBRARY_PATH = "/run/opengl-driver/lib";
-  }
+  },
+}:
+pkgs.mkShell.override { stdenv = pkgs.gcc12Stdenv; } {
+  packages = with pkgs; [
+    cmake
+    cudaPackages.cudatoolkit
+  ];
+  LD_LIBRARY_PATH = "/run/opengl-driver/lib";
+}
